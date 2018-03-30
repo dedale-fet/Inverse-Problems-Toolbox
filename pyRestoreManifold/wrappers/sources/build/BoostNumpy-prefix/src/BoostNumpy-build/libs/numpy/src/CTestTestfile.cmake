12328b1cab4ce3d1cc9f44748b6592b9dd31e501
florentsureau <florent.sureau@cea.fr>
2018-03-30 09:38:18 +0200

    Removed temporary files, added comment

--- pyRestoreManifold/wrappers/sources/build/BoostNumpy-prefix/src/BoostNumpy-build/libs/numpy/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jbobin/Documents/Python/LENA_DEVL/Toolbox/pyWrappers/MacWrappers/pystarlet_manifold/build/BoostNumpy-prefix/src/BoostNumpy/libs/numpy/src
-# Build directory: /Users/jbobin/Documents/Python/LENA_DEVL/Toolbox/pyWrappers/MacWrappers/pystarlet_manifold/build/BoostNumpy-prefix/src/BoostNumpy-build/libs/numpy/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
